813fc0722c85a926b8278d9a8803ee897c7477af
derkl0 <59295086+derkl0@users.noreply.github.com>
2020-10-28 19:43:14 -0400

    think im done

--- A5/a5.js
+++ A5/a5.js
@@ -1,5 +1,4 @@
 function getValues() {
-    console.log("what the fuck is up");
     var xMin = document.getElementById('x-min').value;
     var xMax = document.getElementById('x-max').value;
     var yMin = document.getElementById('y-min').value;
@@ -8,9 +7,23 @@ function getValues() {
     var table = document.getElementById("table");
     var row = table.insertRow();
     var cell = row.insertCell();
-
-    makeTable(Math.round(xMin), Math.round(xMax), Math.round(yMin), Math.round(yMax), table, row, cell);
-    return;
+    
+    table.innerHTML ="";
+    xMinError.innerHTML = "";
+    xMaxError.innerHTML = "";
+    yMinError.innerHTML = "";
+    yMaxError.innerHTML = "";
+    generalError.innerHTML="";
+    
+    if(!(inputCheck(xMin, xMinError))) check = false;
+    if(!(inputCheck(xMax, xMaxError))) check = false;
+    if(!(inputCheck(yMin, yMinError))) check = false;
+    if(!(inputCheck(yMax, yMaxError))) check = false;
+    
+    if(check){   
+        makeTable(Math.round(xMin), Math.round(xMax), Math.round(yMin), Math.round(yMax), table, row, cell);
+        return;
+    }
 
 }
 
@@ -23,6 +36,7 @@ function makeTable(xMin,xMax,yMin,yMax,table,row,cell){
         temp = xMin;
         xMin = xMax;
         xMax = temp;
+        generalError.innerHTML="X-Min and X-Max swapped";
     }
 
     // If the yStart input is > yEnd input, swap
@@ -30,6 +44,10 @@ function makeTable(xMin,xMax,yMin,yMax,table,row,cell){
         temp = yMin;
         yMin = yMax;
         yMax = temp;
+        if(generalError.innerHTML == ""){
+            generalError.innerHTML="Y-Min and Y-Max swapped";
+        }
+        else generalError.innerHTML+="&& Y-Min and Y-Max swapped";
     }
 
 
@@ -54,4 +72,21 @@ function makeTable(xMin,xMax,yMin,yMax,table,row,cell){
             cell.innerHTML = j*i;
         }
     }
+}
+
+function inputCheck(input, error){
+    error.innerHTML = "";
+    if(isNaN(input)){
+        error.innerHTML = "Please only enter numbers";
+        return false;
+    }
+    if(input == ""){
+        error.innerHTML = "Please fill all fields";
+        return false;
+    }
+    if(input > 50 || input < -50){
+        error.innerHTML = "Please provide integers between -50 and 50.";
+        return false;
+    }
+    return true;
 }
